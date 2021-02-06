2b398a97b24fedce63e17746a35205ea9796d92f
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-02-06 18:10:58 -0500

    defining server routes

--- server.js
+++ server.js
@@ -2,3 +2,27 @@ const exrpress = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+
+
+const app = exrpress();
+
+const PORT = process.env.PORT || 8080;
+
+// HTTP request logger
+app.use(morgan('tiny'));
+
+app.get('/api', (req, res) => {
+    const data = {
+        username: 'test',
+        age: 5
+    };
+    res.json(data);
+});
+
+app.get('/api/name', (req, res) => {
+    const data = {
+        username: 'wizzzz',
+        age: 5
+    };
+    res.json(data);
+});
