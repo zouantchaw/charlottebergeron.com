76cd2c880a52e105cffe1da5770004e6696be2b0
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-02-07 15:23:14 -0500

    model abstraction, route abstraction

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = exrpress();
 
 const PORT = process.env.PORT || 8080;
 
+const routes = require('./routes/api');
+
 mongoose.connect(MONGODB_URI || 'mongodb://localhost/cb_website', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -19,54 +21,8 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!!')
 });
 
-// Schema 
-const Schema = mongoose.Schema;
-const EssaySchema = new Schema({
-    title: String,
-    body: String,
-    date: {
-        type: String,
-        default: Date.now()
-    }
-});
-
-//Model 
-const Essay = mongoose.model('Essay', EssaySchema);
-
-//Saving data to mongo db
-const data = {
-    title: 'Welcome to my website',
-    body: "Testing to see if this database connection is working"
-};
-
-const newEssay = new Essay(data); // Instance of model
-newEssay.save((error) => {
-    if (error) {
-        console.log('Ooops, something happened');
-    } else {
-        console.log('Data has been saved!!');
-    }
-})
-// .save();
-
-
 // HTTP request logger
 app.use(morgan('tiny'));
+app.use('/', routes);
 
-app.get('/api', (req, res) => {
-    const data = {
-        username: 'test',
-        age: 5
-    };
-    res.json(data);
-});
-
-app.get('/api/name', (req, res) => {
-    const data = {
-        username: 'wizzzz',
-        age: 5
-    };
-    res.json(data);
-});
-
-app.listen(PORT, console.log(`Server is starting at ${PORT}`))
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
