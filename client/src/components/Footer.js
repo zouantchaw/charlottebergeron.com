fb1e47e24603ce57f0f2ce1295e5691f1f77f13e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-02-15 13:16:50 -0500

    initialize new Footer componenet

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,33 +1,7 @@
 import React from 'react';
-import { Container, Nav, NavItem } from 'react-bootstrap';
-import '../Footer.css';
 
-function Footer(/*props*/) {
-  return (
-    <footer>
-      <Container>
-        <Nav justified>
-          <NavItem
-            eventKey={1}>
-            Privacy policy
-          </NavItem>
-          <NavItem
-            eventKey={2}
-            title="Item"> 
-            Terms & Conditions
-          </NavItem>
-          <NavItem
-            eventKey={3}>
-            Some other professional link
-          </NavItem>
-        </Nav>
-
-        <div className="text-center small copyright">
-          © RLM 2016
-        </div>
-      </Container>
-    </footer>
-  );
+function Footer() {
+  return <div>Lorem Ipsum dolor sit amet.</div>
 }
 
 export default Footer;
