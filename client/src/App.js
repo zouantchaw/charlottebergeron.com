a88a82ec354e9d824f330660454e3a621f546228
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-02-19 12:07:06 -0500

    update footer component stlying

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import BootstrapNavbar from './components/BootstrapNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
-import './App.css';
-
 
 class App extends Component {
   render() {
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,77 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import '../Footer.css';
 
 function Footer() {
   return (
-    <FooterContainer className="main-footer">
-      <div className="footer-middle">
-      <div className="container">
-        <div className="row">
-          {/* Column 1 */}
-          <div className="col-md-3 col-sm-6">
-          <h4>Lorem Ipsum</h4>
-            <ul className="list-unstyled">
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-            </ul>
-          </div>
-          {/* Column 2 */}
-          <div className="col-md-3 col-sm-6">
-          <h4>Lorem Ipsum</h4>
-            <ul className="list-unstyled">
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-            </ul>
-          </div>
-          {/* Column 3 */}
-          <div className="col-md-3 col-sm-6">
-          <h4>Lorem Ipsum</h4>
-            <ul className="list-unstyled">
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-              <li>Lorem, Ipsum</li>
-            </ul>
-          </div>
-        </div>
-        {/* Footer Bottom */}
-        <div className="footer-bottom">
-          <p className="text-xs-center">
-            &copy;{new Date().getFullYear()} Made with love <i class="fas fa-heart"></i>
-          </p>
-        </div>
-      </div>
-      </div>
-    </FooterContainer>
-  );
+    <div className="Footer">
+      Footer
+    </div>
+  )
 }
 
-export default Footer;
-
-const FooterContainer = styled.footer`
-.footer-middle {
-  background: var(--mainDark);
-  padding-top: 3rem;
-  color: var(--mainWhite);
-  text-align: center;
-}
-
-.footer-bottom {
-  padding-top: 1rem;
-  padding-bottom: 2rem;
-  text-align: center;
-  
-}
-
-.main-footer {
-  position:absolute;
-  left:0;
-  bottom:0;
-  right:0;
-  padding-bottom:60px;
-}
-`;
+export default Footer
