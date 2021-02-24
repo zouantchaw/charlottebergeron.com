92ad8fc1daad6f722ff22d4b0b2d8eaab8a3fa22
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-02-23 20:28:46 -0500

    working on nested routes

--- client/src/components/Essays.js
+++ client/src/components/Essays.js
@@ -1,96 +1,35 @@
 import React, { Component } from 'react';
-import {Card, Button } from 'react-bootstrap'
+import { Card, Button, CardColumns } from 'react-bootstrap'
 import '../Box.css'
-import { Container, Row, Col } from 'reactstrap';
+import Link from "react-router-dom";
 
 class Essays extends Component {
 
     renderEssays = () => { 
         console.log(this.props.essays)
         return this.props.essays.map(essay => (
-            <Row>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="/assets/yellowBackground.jpeg" /> */}
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+                <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>{essay.title}</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Card.Link href="#">Read</Card.Link>
+                    {/* <Link to="/test">Read</Link> */}
+                </Card.Body>
+                </Card>
             ));
         };
         
         render() {
             return (
-                <Container>
-                    <div>
-                    <Row>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="/assets/yellowBackground.jpeg" /> */}
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="/assets/yellowBackground.jpeg" /> */}
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="/assets/yellowBackground.jpeg" /> */}
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        {/* <Card.Img variant="top" src="/assets/yellowBackground.jpeg" /> */}
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-                        
-                    </div>
-                </Container>
+                <CardColumns>
+                    {this.renderEssays()}
+                </CardColumns>
         );
     }
 }
-// {this.renderEssays()}
 // <li>{essay.body}</li>
 
 export default Essays;
